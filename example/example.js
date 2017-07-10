9604bc073f3c85fb477fa884895892380773331b
uraway <masato.uraway@gmail.com>
2017-07-10 10:10:12 +0800

    Improve example

--- example/example.js
+++ example/example.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import TwitterTimeline from '../lib/TwitterTimeline';
+import TwitterTimeline from 'react-twitter-embedded-timeline';
 
 class App extends Component {
   render() {
