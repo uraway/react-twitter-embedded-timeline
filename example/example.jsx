83636f130980fba463759df81da5a81567a2554c
uraway <masato.uraway@gmail.com>
2018-01-04 17:02:37 +0900

    Use latest module in example

--- example/example.jsx
+++ example/example.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TwitterTimeline from '../src/TwitterTimeline';
+import TwitterTimeline from 'react-twitter-embedded-timeline';
 
 ReactDOM.render(
-  <div>
-    <TwitterTimeline user="uraway_" />
-    <TwitterTimeline widgetId="695868534455275520" chrome="noborders noheader" />
-  </div>,
+  <TwitterTimeline widgetId="695868534455275520" chrome="noborders noheader" />,
   document.getElementById('app')
 );
